c0e2a9bd19d8d7bc7314d47b4f5dbe4e00696cd1
Sagar <bishalchoudhary67@gmail.com>
2020-10-06 13:41:43 +0530

    initial

---
+++ src/components/Hour.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Hour() {
+  return <div></div>;
+}
+export default Hour;
---
+++ src/components/Minute.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const spanStyle = {
+  color: "green",
+  fontSize: 30
+};
+function Minute() {
+  return <span style={spanStyle}></span>;
+}
+export default Minute;
