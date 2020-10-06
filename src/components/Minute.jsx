76e62987e3408dd32f35210aaa72015ecb5fa055
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-10-06 14:31:08 +0530

    Update Minute.jsx

--- src/components/Minute.jsx
+++ src/components/Minute.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 
-const spanStyle = {
-  color: "green",
-  fontSize: 30
-};
+
 function Minute() {
-  return <span style={spanStyle}></span>;
+  return <span></span>;
 }
 export default Minute;
