86050d781ad183a9b3fec0d7165956d93a85028a
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-10-06 14:29:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
+import Clock from "./Clock";
+import Hour from "./Hour";
+import Minute from "./Minute";
+
+
+const clockStyle = {
+    display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+};
+
+const minuteStyle = {
+    color: "green",
+    fontSize: 30,
+  };
 
 class App extends Component {
     render() {
 
         return(
             <>
-               <Clock/>
+               <Clock style={clockStyle}>
+                   <Hour/>
+                   <Minute style={minuteStyle}/>
+               </Clock>
             </>
         )
     }
