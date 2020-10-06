7e1ebe0808f453ce96e76c78ee8508ee57a51d7d
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-10-06 14:30:30 +0530

    Update Clock.jsx

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,18 +1,8 @@
 import React from "react";
-import Hour from "./Hour";
-import Minute from "./Minute";
-
-const customStyle = {
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "center"
-};
 
 function Clock() {
   return (
-    <div style={customStyle}>
-      <Hour />
-      <Minute />
+    <div>
     </div>
   );
 }
