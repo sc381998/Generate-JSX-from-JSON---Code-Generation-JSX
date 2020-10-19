96caeba9e8d79e59d01b9039276b9f3a6be65046
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-10-20 01:20:24 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 import Clock from "./Clock";
 import Hour from "./Hour";
 import Minute from "./Minute";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               <Clock>
-                   <Hour />
-                   <Minute />
-               </Clock>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Clock>
+          <Hour />
+          <Minute />
+        </Clock>
+      </>
+    );
+  }
 }
-
 
 export default App;
