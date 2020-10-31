da6b4e1d4e7fb9adebb0d090666685f2b433273d
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-10-31 13:55:18 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,58 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import Clock from "./Clock";
-import Hour from "./Hour";
-import Minute from "./Minute";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.convertJSONtoJSX = this.convertJSONtoJSX.bind(this);
+  }
+  data = {
+    type: "div",
+    name: "Clock",
+    root: true,
+    style: {
+      display: "flex",
+      "flex-direction": "row",
+      "justify-content": "center"
+    },
+    children: [
+      {
+        type: "div",
+        name: "Hour",
+        style: {},
+        children: []
+      },
+      {
+        type: "span",
+        name: "Minute",
+        style: {
+          color: "green",
+          "font-size": 30
+        },
+        children: []
+      }
+    ]
+  };
+
+  convertJSONtoJSX(obj, arr) {
+    if (!Array.isArray(obj.children) && !obj.children.length) {
+      arr.push("</" + obj.name + ">");
+    }
+    if (obj.hasOwnProperty("name")) {
+      arr.push(
+        "<" + obj.name + " type=" + obj.type + " style=" + obj.style + ">"
+      );
+    }
+    if (obj.hasOwnProperty("children")) {
+      for (let i = 0; i < obj.children.length; i++) {
+        this.convertJSONtoJSX(obj.children[i], arr);
+      }
+    }
+    return arr;
+  }
+  arr = [];
   render() {
-    return (
-      <>
-        <Clock>
-          <Hour />
-          <Minute />
-        </Clock>
-      </>
-    );
+    return <>{this.convertJSONtoJSX(this.data, this.arr).map((ele) => ele)}</>;
   }
 }
 
--- src/components/Hour.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-function Hour() {
-  return <div></div>;
-}
-export default Hour;
