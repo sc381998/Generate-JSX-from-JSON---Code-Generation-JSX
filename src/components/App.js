a172f45bd753457927dfb75f60ae3c81ed12aeff
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-11-02 00:39:34 +0530

    added the function jsonToJsx

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
-class App extends Component {}
+class App extends Component {
+  render() {
+    return <></>;
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj) {
-  let ans_str = "<";
-  ans_str += obj.name + "";
-  if (obj.style !== undefined && Object.keys(obj.style).length > 0) {
-    ans_str += " style{{";
-    let style_keys = Object.keys(obj.style);
-    for (let i = 0; i < Object.keys(obj.style).length; i++) {
-      ans_str += style_keys[i] + ':"' + obj.style[style_keys[i]] + '",';
-    }
-    ans_str = ans_str.substr(0, ans_str.length - 1) + "}}";
-  }
-  //console.log(obj.children!=undefined && obj.children.length>0);
-  if (obj.children !== undefined && obj.children.length > 0) {
-    ans_str += ">\n";
-    obj.children.forEach((ele) => {
-      ans_str += generateCodeFromObject(ele);
+const title = (str) => {
+  return str
+    .replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    })
+    .replace(" ", "");
+};
+
+const camelCase = (str) => {
+  str = str.replace("-", " ");
+  return str
+    .replace(/\s(.)/g, function (s1) {
+      return s1.toUpperCase();
+    })
+    .replace(/\s/g, "")
+    .replace(/^(.)/, function (s1) {
+      return s1.toLowerCase();
     });
-    ans_str += "</" + obj.name + ">\n";
-  } else {
-    ans_str += ">\n";
+};
+
+const jsonToJsx = (obj) => {
+  let ansStr = "<";
+  ansStr += title(obj.name) + " ";
+  if (obj.style !== undefined && Object.keys(obj.style).length > 0) {
+    var stKeys = Object.keys(obj.style);
+    ansStr += "style={{";
+    for (var i = 0; i < stKeys.length; i++) {
+      ansStr += camelCase(stKeys[i]) + ':"' + obj.style[stKeys[i]] + '",';
+    }
+    ansStr = ansStr.substr(0, ansStr.length - 1) + "}}";
   }
-  return ans_str;
+
+  if (obj.children !== undefined && obj.children.length > 0) {
+    ansStr += ">\n";
+    for (var i = 0; i < obj.children.length; i++) {
+      ansStr += jsonToJsx(obj.children[i]);
+    }
+    ansStr += "</" + title(obj.name) + ">";
+  } else {
+    ansStr += "/>";
+  }
+
+  return ansStr;
+};
+
+function generateCodeFromObject(obj) {
   //return a code generated string
+  return jsonToJsx(obj);
 }
 
 module.exports = generateCodeFromObject;
